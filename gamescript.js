2a3a0768663634d046dd78c6e1919fba2fcc0b9c
rcoderx <104567101+rcoderx@users.noreply.github.com>
2023-11-26 07:30:26 -0600

    Add files via upload

---
+++ gamescript.js
@@ -0,0 +1,371 @@
+const canvas = document.getElementById('gameCanvas');
+canvas.addEventListener('touchmove', function(e) {
+    e.preventDefault(); // Prevent the default touch action (scrolling, zooming, etc.)
+}, { passive: false }); // Setting passive to false allows preventDefault to work
+
+const ctx = canvas.getContext('2d');
+const grid = 20;
+let speed = 2;
+
+let bobbingAmount = 0;
+let bobbingDirection = 1;
+let highScore = localStorage.getItem('highScore') || 0;
+// Load the images
+let snakeImage = new Image();
+const selectedAvatar = localStorage.getItem('selectedAvatar') || 'snake.png'; // Default to 'avatar1.png' if no avatar is selected
+snakeImage.src = selectedAvatar;
+const birdImage = new Image();
+birdImage.src = 'bird.png'; // Replace with the actual path
+
+// Sound effects
+const eatSound = new Audio('eatsound.mp3');
+const gameOverSound = new Audio('gameover.wav');
+
+// Background music
+const backgroundMusic = new Audio('background.mp3');
+backgroundMusic.loop = true;
+
+const startButton = document.getElementById('startButton');
+const restartButton = document.getElementById('restartButton');
+
+let gameInProgress = false;
+let snake = [{ x: 160, y: 160 }, { x: 140, y: 160 }, { x: 120, y: 160 }];
+let dx = grid;
+let dy = 0;
+let food = { x: Math.floor(Math.random() * (canvas.width / grid)) * grid, y: Math.floor(Math.random() * (canvas.height / grid)) * grid };
+let score = 0;
+
+// Animation frame for smoother animation
+let animationFrameId;
+
+let lastRenderTime = 0;
+ // Ensure this is set correctly in your script
+
+ function main(currentTime) {
+    if (!gameInProgress) return;
+    
+    window.requestAnimationFrame(main);
+    const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000;
+    if (secondsSinceLastRender < 1 / speed) return;
+
+    lastRenderTime = currentTime;
+
+    clearCanvas();
+    drawWalls();
+    drawFood();
+    advanceSnake();
+    drawSnake();
+
+    // Update bobbing logic
+    bobbingAmount += 0.2 * bobbingDirection;
+    if (bobbingAmount > 5 || bobbingAmount < -5) {
+        bobbingDirection *= -1;
+    }
+
+    if (didGameEnd()) {
+        gameOverSound.play();
+        backgroundMusic.pause();
+        restartButton.style.display = 'block';
+        startButton.style.display = 'block';
+        gameInProgress = false;
+        return;
+    }
+}
+
+
+window.requestAnimationFrame(main); // Start the game loop
+
+
+    clearCanvas();
+    drawWalls();
+    drawFood();
+    advanceSnake();
+    drawSnake();
+
+    animationFrameId = requestAnimationFrame(main);
+
+// Start and Restart Button Event Listeners
+startButton.addEventListener('click', function() {
+    gameInProgress = true;
+    startButton.style.display = 'none';
+    backgroundMusic.play(); // Play the background music
+    window.requestAnimationFrame(main);
+});
+
+
+restartButton.addEventListener('click', function() {
+    resetGame();
+    gameInProgress = true;
+    restartButton.style.display = 'none';
+    backgroundMusic.play(); // Play the background music
+    window.requestAnimationFrame(main);
+});
+
+
+function resetGame() {
+    snake = [
+        { x: 160, y: 160 },
+        { x: 140, y: 160 },
+        { x: 120, y: 160 }
+    ];
+    dx = grid;
+    dy = 0;
+    score = 0;
+    document.getElementById('score').innerHTML = score;
+    createFood(); // Create initial food
+}
+
+// Function to toggle background music
+function toggleMusic() {
+    if (backgroundMusic.paused) {
+        backgroundMusic.play();
+    } else {
+        backgroundMusic.pause();
+    }
+}
+
+function resetGame() {
+    snake = [{ x: 160, y: 160 }, { x: 140, y: 160 }, { x: 120, y: 160 }];
+    dx = grid;
+    dy = 0;
+    score = 0;
+    document.getElementById('score').innerHTML = score;
+    createFood(); // Create initial food
+}
+
+function clearCanvas() {
+    ctx.fillStyle = 'black';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+}
+
+function drawFood() {
+    // Adjust the bird's vertical position based on bobbingAmount
+    const birdYPosition = food.y + bobbingAmount;
+
+    // Draw the bird with the adjusted Y position
+    ctx.drawImage(birdImage, food.x, birdYPosition, grid, grid);
+}
+
+
+
+function advanceSnake() {
+    let nextHeadX = snake[0].x + dx;
+    let nextHeadY = snake[0].y + dy;
+
+    // Pass through walls: reappear on the opposite side
+    if (nextHeadX >= canvas.width) {
+        nextHeadX = 0;
+    } else if (nextHeadX < 0) {
+        nextHeadX = canvas.width - grid;
+    }
+
+    if (nextHeadY >= canvas.height) {
+        nextHeadY = 0;
+    } else if (nextHeadY < 0) {
+        nextHeadY = canvas.height - grid;
+    }
+
+    const head = { x: nextHeadX, y: nextHeadY };
+
+    // Check if the snake has eaten the food
+    const didEatFood = head.x === food.x && head.y === food.y;
+    if (didEatFood) {
+        score += 10;
+        document.getElementById('score').innerHTML = score;
+        createFood();
+        speed += 0.5;
+        eatSound.play();  // Play eating sound
+    } else {
+        snake.pop();
+    }
+
+    snake.unshift(head);  // Add the new head to the snake
+}
+function createFood() {
+    food = {
+        x: Math.floor((Math.random() * ((canvas.width - grid) / grid))) * grid,
+        y: Math.floor((Math.random() * ((canvas.height - grid) / grid))) * grid
+    };
+
+    // Check if food is spawned on the snake
+    snake.forEach(function(part) {
+        const foodIsOnSnake = part.x == food.x && part.y == food.y;
+        if (foodIsOnSnake)
+            createFood();
+    });
+}
+
+
+function drawSnake() {
+    snake.forEach(function(snakePart, index) {
+        // Use the snake image for each part of the snake
+        ctx.drawImage(snakeImage, snakePart.x, snakePart.y, grid, grid);
+    });
+}
+
+function didGameEnd() {
+    // Check self-collision
+    for (let i = 4; i < snake.length; i++) {
+        if (snake[i].x === snake[0].x && snake[i].y === snake[0].y) return true;
+    }
+    if (gameOver) {
+        checkAndUpdateHighScore(); // Update the high score if necessary
+    }
+    // Game doesn't end by hitting walls anymore
+    return false;
+    if (gameOver) {
+        gameOverSound.play();  // Play game over sound
+    }
+
+    return gameOver;
+}
+
+
+function drawWalls() {
+    ctx.strokeStyle = 'Red';
+    ctx.lineWidth = 15;
+    ctx.strokeRect(0, 0, canvas.width, canvas.height);
+}
+
+function changeDirection(event) {
+    const keyPressed = event.keyCode;
+    const LEFT_KEY = 37, RIGHT_KEY = 39, UP_KEY = 38, DOWN_KEY = 40;
+
+    const goingUp = dy === -grid;
+    const goingDown = dy === grid;
+    const goingRight = dx === grid;
+    const goingLeft = dx === -grid;
+
+    if (goingDown && (keyPressed === UP_KEY || keyPressed === DOWN_KEY)) {
+        // Ignore up or down key when going down
+        return;
+    }
+
+    if ((keyPressed === LEFT_KEY) && !goingRight) {
+        dx = -grid;
+        dy = 0;
+    } else if ((keyPressed === UP_KEY) && !goingDown) {
+        dx = 0;
+        dy = -grid;
+    } else if ((keyPressed === RIGHT_KEY) && !goingLeft) {
+        dx = grid;
+        dy = 0;
+    } else if ((keyPressed === DOWN_KEY) && !goingUp) {
+        dx = 0;
+        dy = grid;
+    }
+}
+
+// Other functions like clearCanvas, drawFood, advanceSnake, drawSnake, didGameEnd, etc. remain the same
+
+// Add touch controls for mobile responsiveness
+canvas.addEventListener('touchstart', handleTouchStart, false);
+canvas.addEventListener('touchmove', handleTouchMove, false);
+
+let xDown = null;
+let yDown = null;
+
+function handleTouchStart(evt) {
+    const firstTouch = evt.touches[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if (!xDown || !yDown) {
+        return;
+    }
+
+    let xUp = evt.touches[0].clientX;
+    let yUp = evt.touches[0].clientY;
+    let xDiff = xDown - xUp;
+    let yDiff = yDown - yUp;
+
+    if (Math.abs(xDiff) > Math.abs(yDiff)) {
+        if (xDiff > 0 && dx === 0) { dx = -grid; dy = 0; } // Left swipe
+        else if (xDiff < 0 && dx === 0) { dx = grid; dy = 0; } // Right swipe
+    } else {
+        if (yDiff > 0 && dy === 0) { dy = -grid; dx = 0; } // Up swipe
+        else if (yDiff < 0 && dy === 0) { dy = grid; dx = 0; } // Down swipe
+    }
+
+    xDown = null;
+    yDown = null;
+};
+
+// Use local storage to save and display high scores
+function saveHighScore(score) {
+    const highScore = localStorage.getItem('highScore') || 0;
+    if (score > highScore) {
+        localStorage.setItem('highScore', score);
+        // Display the new high score
+    }
+    function handleError(error) {
+        console.error('An error occurred:', error);
+        // Display error to the user or take appropriate actions
+    }
+    
+}
+function checkAndUpdateHighScore() {
+    if (score > highScore) {
+        highScore = score;
+        localStorage.setItem('highScore', highScore);
+        document.getElementById('highScore').innerText = highScore;
+    }
+    function handleError(error) {
+        console.error('An error occurred:', error);
+        // Display error to the user or take appropriate actions
+    }
+    
+}
+function updateLeaderboard() {
+    fetch('https://psychic-chainsaw-production.up.railway.app/leaderboard')
+        .then(response => response.json())
+        .then(leaderboardData => {
+            let leaderboardHTML = '';
+            leaderboardData.forEach(player => {
+                leaderboardHTML += `<p>${player.address}: ${player.score}</p>`;
+            });
+            document.getElementById('leaderboard').innerHTML = leaderboardHTML;
+        })
+        .catch(error => console.error('Error fetching leaderboard:', error));
+}
+function updateLives(lives) {
+    document.getElementById('livesCount').innerText = lives;
+}
+function checkLivesAndStartGame() {
+    const walletAddress = localStorage.getItem('walletAddress');
+    fetch(`https://psychic-chainsaw-production.up.railway.app/getLives?address=${walletAddress}`)
+        .then(response => response.json())
+        .then(data => {
+            if (data.lives > 0) {
+                startGame(); // Function that starts the game
+            } else {
+                alert("You have no lives left. Share on Twitter for more lives!");
+                // Logic to direct the user to get more lives (e.g., show Twitter share button)
+            }
+        })
+        .catch(error => {
+            console.error('Error checking lives:', error);
+            alert('Error checking lives. Please try again.');
+        });
+}
+
+// Update this whenever lives change
+
+
+// Call this function at appropriate times, such as after each game
+
+document.addEventListener("keydown", changeDirection);
+// Load and display the high score on game start
+window.onload = function() {
+    document.getElementById('highScore').innerText = highScore;
+    const highScore = localStorage.getItem('highScore') || 0;
+    // Display the high score somewhere in your UI
+}
+
+// Comment or remove auto-start game loop
+// main(); // This will now be started by the Start button
+
+// Optionally, start the background music when the game starts
+// backgroundMusic.play();
---
+++ script.js
@@ -0,0 +1,154 @@
+// Example of using Ethereum wallet connection (e.g., MetaMask)
+document.getElementById('connectWallet').addEventListener('click', function() {
+    if (typeof window.ethereum !== 'undefined') {
+        window.ethereum.request({ method: 'eth_requestAccounts' })
+        .then(function(accounts) {
+            if (accounts.length > 0) {
+                const walletAddress = accounts[0];
+                alert('Wallet connected: ' + walletAddress);
+                // Store the connected wallet address for later use
+                localStorage.setItem('walletAddress', walletAddress);
+            } else {
+                alert('No wallet addresses found.');
+            }
+        })
+        .catch(function(error) {
+            console.error("Error connecting to wallet: ", error);
+        });
+    } else {
+        alert("Ethereum wallet not found. Please install MetaMask.");
+    }
+});
+
+// Additional script setup...
+
+function submitScore(score) {
+    const address = localStorage.getItem('walletAddress');
+    fetch('https://psychic-chainsaw-production.up.railway.app/updateScore', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ address, score })
+    }).then(response => response.json())
+    .then(data => console.log('Score updated:', data))
+    .catch(err => console.error('Error updating score:', err));
+}
+
+function verifyTwitterLink(url) {
+    const twitterUrlPattern = /^https?:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/[0-9]+$/;
+    return twitterUrlPattern.test(url);
+}
+
+document.getElementById('twitterShareBtn').addEventListener('click', function() {
+    // Logic to share the game link on Twitter
+    this.disabled = true; // Disable the button after clicking
+});
+function handleTwitterShare() {
+    // Construct the Twitter share URL
+    var url = "https://twitter.com/intent/tweet";
+    var text = "Check out SnekCoin Saga, an awesome blockchain game!"; // Your share message
+    var hashtags = "SnekCoinSaga,$Hiss"; // Relevant hashtags
+
+    var twitterUrl = `${url}?text=${encodeURIComponent(text)}&hashtags=${encodeURIComponent(hashtags)}`;
+    window.open(twitterUrl, '_blank').focus();
+
+    // Disable the share button after clicking
+    document.getElementById('twitterShareBtn').disabled = true;
+}
+
+
+function submitTwitterLink() {
+    const twitterLink = document.getElementById('twitterLinkInput').value;
+    if (verifyTwitterLink(twitterLink)) {
+        // Send request to server to update lives
+        fetch('https://psychic-chainsaw-production.up.railway.app/updateLives', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ address: localStorage.getItem('walletAddress'), lives: 10 })
+        }).then(() => {
+            alert('Lives updated successfully');
+        }).catch(err => console.error('Error updating lives:', err));
+    } else {
+        alert('Invalid Twitter link');
+    }
+}
+function verifyAndSubmitTwitterLink() {
+    const twitterLink = document.getElementById('twitterLinkInput').value;
+    if (verifyTwitterLink(twitterLink)) {
+        fetch('/updateLives', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ address: localStorage.getItem('walletAddress'), lives: 10 })
+        }).then(() => {
+            alert('Lives updated successfully');
+        }).catch(handleError);
+    } else {
+        alert('Invalid Twitter link');
+    }
+}
+
+function verifyTwitterLink(url) {
+    // Define multiple patterns for valid Twitter URLs
+    const patterns = [
+        /^https?:\/\/twitter\.com\/[a-zA-Z0-9_]+\/status\/[0-9]+$/, // Standard Twitter status URL
+        /^https?:\/\/x\.com\/[a-zA-Z0-9_]+$/ // Additional pattern (e.g., x.com)
+    ];
+
+    // Check if the URL matches any of the defined patterns
+    return patterns.some(pattern => pattern.test(url));
+}
+
+function handleError(error) {
+    console.error('An error occurred:', error);
+    alert('An error occurred. Please try again.');
+}
+
+// Function to call submitScore after each game...
+
+
+// Function to retrieve the leaderboard from the server
+function getLeaderboard() {
+    fetch('https://psychic-chainsaw-production.up.railway.app/leaderboard')
+        .then(response => response.json())
+        .then(leaderboardData => {
+            let leaderboardHTML = '<h3>Leaderboard</h3>';
+            leaderboardData.forEach(player => {
+                leaderboardHTML += `<p>${player.address}: ${player.score}</p>`;
+            });
+            document.getElementById('leaderboard').innerHTML = leaderboardHTML;
+        })
+        .catch(error => console.error('Error fetching leaderboard:', error));
+}
+function updateLives() {
+    const walletAddress = localStorage.getItem('walletAddress');
+    fetch('https://psychic-chainsaw-production.up.railway.app/getLives?address=' + walletAddress)
+        .then(response => response.json())
+        .then(data => {
+            if (data.lives > 0) {
+                console.log('Lives remaining:', data.lives);
+            } else {
+                console.error('No lives remaining');
+                // Additional logic when player runs out of lives
+            }
+        })
+        .catch(error => console.error('Error checking lives:', error));
+}
+function updateScore(score) {
+    const walletAddress = localStorage.getItem('walletAddress');
+    fetch('https://psychic-chainsaw-production.up.railway.app/updateScore', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ address: walletAddress, score: score })
+    })
+    .then(response => {
+        if (response.ok) {
+            console.log('Score updated successfully');
+        } else {
+            console.error('Failed to update score');
+        }
+    })
+    .catch(error => console.error('Error updating score:', error));
+}
+
+
+// Call this function to display the leaderboard on page load
+getLeaderboard();
